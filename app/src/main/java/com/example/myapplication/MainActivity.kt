a42fb9e0c52beb32e7932f52655b1f94b522d701
4us228 <4us.in.ua@gmail.com>
2021-08-14 17:38:22 +0300

    2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.hw_project_2)
     }
 }
